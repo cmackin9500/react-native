225f16605adecc988d272c9587fb0c318accca68
cmackin9500 <casey@mackinnon.org>
2024-03-11 21:29:15 -0600

    adding modal

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import {
   StyleSheet,
-  Image,
   View,
   Text,
   FlatList,
   TouchableOpacity,
-  ImageBackground,
+  Modal,
 } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { globalStyles } from "../styles/global";
 import Card from "./../shared/card";
 
 export default function Home({ navigation }) {
+  const [modalOpen, setModalOpen] = useState(false);
   const [players, setPlayers] = useState([
     { name: "Kulusevski", foot: "Left", Height: "6.1", rating: 3, key: 1 },
     { name: "Madders", foot: "Right", Height: "5.9", rating: 4, key: 2 },
@@ -21,6 +22,24 @@ export default function Home({ navigation }) {
 
   return (
     <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType="slide">
+        <View style={{ ...styles.modalToggle, ...styles.modalClose }}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            onPress={() => setModalOpen(false)}
+          />
+          <Text>Hey</Text>
+        </View>
+      </Modal>
+
+      <MaterialIcons
+        name="add"
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)}
+      />
+
       <FlatList
         data={players}
         renderItem={({ item }) => (
@@ -47,5 +66,14 @@ const styles = StyleSheet.create({
     // remove width and height to override fixed static size
     width: null,
     height: null,
+  },
+  modalClose: { marginTop: 40, marginBottom: 0 },
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#f2f2f2",
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: "center",
   },
 });
